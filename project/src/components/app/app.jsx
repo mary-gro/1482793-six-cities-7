cfaf2153ad16b66fad686c3408b006c315453117
Мария Громова <gromova.marija.i@gmail.com>
2021-06-05 15:25:13 +0300

    Добавляет маршрутизацию

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Main from '../main/main.jsx';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import MainScreen from '../main-screen/main-screen.jsx';
+import FavoritesScreen from '../favorites-screen/favorites-screen.jsx';
+import LoginScreen from '../login-screen/login-screen.jsx';
+import RoomScreen from '../room-screen/room-screen.jsx';
+import NotFoundScreen from '../not-found-screen/not-found-screen.jsx';
+import {AppRoute} from '../../const.js';
 
 function App(props) {
   const {cardsCount} = props;
 
-  return <Main cardsCount={cardsCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.MAIN}>
+          <MainScreen
+            cardsCount={cardsCount}
+          />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <FavoritesScreen />
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <LoginScreen />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <RoomScreen />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
